79b0e27c3702b567693058b944add6e737de451c
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2025-12-28 14:36:29 +0100

    Update adverts.tsx

--- adverts.tsx
+++ adverts.tsx
@@ -43,7 +43,7 @@ export default function Adverts() {
       px={4}
       py={2}
       my={2}
-      mx={{md:"8em"}}
+      mx={{md:"18em"}}
       css={{
         '&::-webkit-scrollbar': {
           display: 'none',
