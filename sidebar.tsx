5a1f520e249f3d9bc9e4de96cb2f323d50697429
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2025-12-28 08:46:12 +0100

    Adjust right position of toggle button in sidebar

--- sidebar.tsx
+++ sidebar.tsx
@@ -61,7 +61,7 @@ const Sidebar = () => {
           onClick={onToggle}
           position="fixed"
           top="3"
-          right="18"
+          right="2em"
           zIndex="modal"
           bgGradient="linear(to-r, purple.300, purple.400)"
           color="white"
