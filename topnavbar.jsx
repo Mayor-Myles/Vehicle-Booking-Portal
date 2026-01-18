72ca76152748054e2e9b6eda2bd7b541d9c31223
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-01-18 15:34:04 +0100

    Update topnavbar.jsx

--- topnavbar.jsx
+++ topnavbar.jsx
@@ -70,7 +70,7 @@ const {colorMode, toggleColorMode} = useColorMode();
           {/* Actions */}
           <HStack spacing={3}>
 
-           <IconButton size="lg" onClick={toggleColorMode} variant="ghost" icon={colorMode ==="light" ? <CiDark fontSize="12px" /> : <CiLight fontSize="12px" />} />
+           <IconButton size="lg" onClick={toggleColorMode} variant="ghost" icon={colorMode ==="light" ? <CiDark size="16px" /> : <CiLight size="16px" />} />
             
             <Button
               size="sm"
