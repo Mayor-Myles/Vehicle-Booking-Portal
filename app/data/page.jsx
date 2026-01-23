77f75f6432f7dd7ca754dec2f4e4c09646d7f49f
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-01-23 22:51:45 +0100

    Fix plans mapping to handle undefined networkSelected

--- app/data/page.jsx
+++ app/data/page.jsx
@@ -106,7 +106,7 @@ export default function DataAirtimePage() {
         </Flex>
 
         <Grid templateColumns="repeat(3, 1fr)" gap={4} mb={6}  >
-          {plans[networkSelected].map((plan, i) => (
+          {plans[networkSelected] || [].map((plan, i) => (
             <Box
               key={i}
               display={selectedPlan?.size ? "none" : "flex"}
