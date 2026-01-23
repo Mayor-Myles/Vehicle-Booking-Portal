316679af6d5ccafa69ddabf65c5a35e5ca803037
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-01-23 12:59:44 +0100

    Fix visibility and button click handler in page.jsx

--- app/data/page.jsx
+++ app/data/page.jsx
@@ -79,7 +79,7 @@ export default function DataAirtimePage() {
         </HStack>
 
         {/* Recipient Number */}
-        <Box visibility={selectedPlan ? "none" : "" } p={4} borderRadius="2xl" mb={6} boxShadow="sm">
+        <Box visibility={selectedPlan ? "none" : "block" } p={4} borderRadius="2xl" mb={6} boxShadow="sm">
           <Text fontSize="sm" color="gray.500" mb={2}>
             RECIPIENT NUMBER
           </Text>
@@ -206,7 +206,7 @@ export default function DataAirtimePage() {
             Proceed →
           </Button>
 
-          <Button onClick={()=>setSeectedPlan(null)}  bg="gray.50" size="lg">Cancel</Button>
+          <Button ml={10} onClick={()=>setSelectedPlan(null)}  bg="gray.50" size="lg">Cancel</Button>
         </Flex>
       </Box>
       <BottomNav />
