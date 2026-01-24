965bf19407876646fc6834cebe6c13ec43c01b9b
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-01-24 09:40:22 +0100

    Update spinner.jsx

--- spinner.jsx
+++ spinner.jsx
@@ -44,8 +44,8 @@ export default function SpinnerEffect() {
               thickness="4px"
               speed="0.65s"
               emptyColor="gray.200"
-              color="blue.500"
-              size="xl"
+              color="purple.500"
+              size="2xl"
               mb={4}
             />
             <Box color="white" fontWeight="medium">
