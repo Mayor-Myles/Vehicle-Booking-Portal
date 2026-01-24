572b08a9f923f73784627a349774f50c39abe715
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-01-24 07:37:21 +0100

    Add RootLayout component with ChakraProvider

--- app/data/layout.tsx
+++ app/data/layout.tsx
@@ -1,1 +1,19 @@
 
+"use client";
+
+import { ChakraProvider } from '@chakra-ui/react';
+
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
+      <body>
+        
+        <ChakraProvider>{children}</ChakraProvider>
+          
+      </body>
+    </html>
+  );
+}
