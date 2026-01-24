8550380296fa62c48efe134ca2e1211a452beaa1
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-01-24 17:59:54 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,19 @@
 
 import React from 'react';
 import { ChakraProvider, Box, Flex, useDisclosure, useBreakpointValue, VStack,useColorModeValue, } from '@chakra-ui/react';
-import { TopNav } from '@/topnav';
-import { Sidebar } from '@/sidebar';
-import Wallet from '@/wallet';
-import Stats from '@/stats';
-import Menu from '@/menu';
-import BottomNav from "@/bottomNav";
-import HeroSection from "@/hero"
-import Services from "@/services"
-import TopNavbar from "@/topnavbar"
-import Home from "@/home";
-import Projects from "@/projects";
-import ContactUs from "@/contact";
-import Footer from "@/footer";
+import  Navbar from '@/topnavbar';
+//import { Sidebar } from '@/sidebar';
+//import Wallet from '@/wallet';
+//import Stats from '@/stats';
+//import Menu from '@/menu';
+//import BottomNav from "@/bottomNav";
+import Hero from "@/hero"
+//import Services from "@/services"
+//import TopNavbar from "@/topnavbar"
+//import Home from "@/home";
+//import Projects from "@/projects";
+//import ContactUs from "@/contact";
+//import Footer from "@/footer";
 
 
 
@@ -29,11 +29,8 @@ const bg = useColorModeValue("gray.50","black");
 
       
       < TopNavbar />
-      <HeroSection />
-      <Services />
-      <Projects />
-      <ContactUs />
-      <Footer />
+      <Hero />
+      
       
       {/*  <Box  minH="100vh"  ml={{"base":2,"md":280}}>
               {!isMobile && <Sidebar isOpen={true} onClose={onClose}   />}
