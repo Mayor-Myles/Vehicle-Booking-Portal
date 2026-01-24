9b29541a40821faa189f7723a5c1b2e1febdf950
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-01-24 18:36:49 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,17 @@
 import React from 'react';
 import { ChakraProvider, Box, Flex, useDisclosure, useBreakpointValue, VStack,useColorModeValue, } from '@chakra-ui/react';
 import  Navbar from '@/topnavbar';
-//import { Sidebar } from '@/sidebar';
-//import Wallet from '@/wallet';
-//import Stats from '@/stats';
-//import Menu from '@/menu';
-//import BottomNav from "@/bottomNav";
 import Hero from "@/hero"
-//import Services from "@/services"
-//import TopNavbar from "@/topnavbar"
-//import Home from "@/home";
-//import Projects from "@/projects";
-//import ContactUs from "@/contact";
-//import Footer from "@/footer";
+import Testimonial from "@/components/testimonial";
+import ContactUs from "@/components/contactUs";
+import SearchForm from "@/components/search";
+import PopularRoutes from "@/components/popular";
+import HowItWorks from "@/components/howItWorks";
+import Features from "@/components/features";
+import FAQ from "@/components/faq";
+
+
+
 
 
 
@@ -25,25 +24,15 @@ function App() {
 const bg = useColorModeValue("gray.50","black");
   
   return (
-    <ChakraProvider>
-
-      
+    <ChakraProvider>      
       < TopNavbar />
       <Hero />
-      
-      
-      {/*  <Box  minH="100vh"  ml={{"base":2,"md":280}}>
-              {!isMobile && <Sidebar isOpen={true} onClose={onClose}   />}
-          {isMobile && <Sidebar isOpen={isOpen} onClose={onClose}  />}
-        
-     
-        <TopNav openSidebar={onOpen} />
-          <Wallet />
-          <Stats/>  
-          <Menu />
-          <BottomNav />
-      </Box>*/}
-      
+      <SearchForm />
+      <PopularRoutes />
+      <HowItWorks />
+      <Features />
+      <FAQ />
+      <ContactUs />  
     </ChakraProvider>
   );
 }
