8bee080ca0bf4206fd6c4f8ac407530fedce6fdb
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-01-24 18:12:34 +0100

    Update search.tsx

--- components/search.tsx
+++ components/search.tsx
@@ -1,1 +1,16 @@
+import { Box, Input, Button, SimpleGrid } from "@chakra-ui/react";
+import { CiLocationOn } from "react-icons/ci";
+
+export default function SearchForm() {
+  return (
+    <Box mt={6} bg="white" p={6} borderRadius="md" color="black" boxShadow="md">
+      <SimpleGrid columns={{ base: 1, md: 4 }} spacing={4}>
+        <Input placeholder="From" icon={<CiLocationOn />} />
+        <Input placeholder="To" icon={<CiLocationOn />} />
+        <Input type="date" placeholder="Date" />
+        <Button colorScheme="orange">Search</Button>
+      </SimpleGrid>
+    </Box>
+  );
+}
 
