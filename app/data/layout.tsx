7dac669c5cf338c1fd306df9a520532fdf1526d2
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-01-24 07:24:12 +0100

    Remove RootLayout component from layout.tsx
    Removed the RootLayout component and its dependencies.

--- app/data/layout.tsx
+++ app/data/layout.tsx
@@ -1,14 +1,1 @@
 
-"use client";
-
-import { RecoilRoot } from "recoil";
-
-export default function RootLayout({ children }) {
-  return (
-    <html>
-      <body>
-        <RecoilRoot>{children}</RecoilRoot>
-      </body>
-    </html>
-  );
-}
