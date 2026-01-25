716858e20e0930e5fc392e3f8312b85377ebea1f
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-01-25 23:40:35 +0100

    Update layout.tsx

--- app/bus-listing/layout.tsx
+++ app/bus-listing/layout.tsx
@@ -1,1 +1,27 @@
 
+"use client";
+
+import { ChakraProvider } from '@chakra-ui/react';
+import Spinner from "@/spinner";
+import {Suspense} from "react";
+
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+
+      <body>
+        
+        <ChakraProvider>
+<Suspense fallback={null}>
+          <Spinner />
+
+          {children}
+</Suspense>
+        </ChakraProvider>
+          
+      </body>
+    </html>
+  );
+}
