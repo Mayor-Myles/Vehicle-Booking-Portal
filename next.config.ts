1b50782b110a7970c6d32fcd896e097ac1f23bdd
Mayor-Myles <162783782+Mayor-Myles@users.noreply.github.com>
2026-02-15 10:40:48 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+async rewrites() {
+    return [
+      {
+        source: '/api/php/:path*',
+        destination: 'http://mylezic.myartsonline.com*', // Your HTTP PHP URL
+      },
+    ]
+  },
+
+  
 };
 
 export default nextConfig;
